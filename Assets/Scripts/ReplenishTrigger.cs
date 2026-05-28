8a7c671dff7f0ae45f6e76a5cdbd81a59b0594f6
cheva360 <sebastianmagana360@gmail.com>
2026-05-27 19:10:18 -0700

    fade out ease out shadergraph

--- Assets/Scripts/ReplenishTrigger.cs
+++ Assets/Scripts/ReplenishTrigger.cs
@@ -4,14 +4,25 @@ public class ReplenishTrigger : MonoBehaviour
 {
     [Tooltip("Amount of stamina to restore on contact.")]
     public float replenishAmount = 100f;
+    [SerializeField] private bool hasBeenUsed = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !hasBeenUsed)
         {
+            hasBeenUsed = true;
             GameController.Instance.StaminaBar.ReplenishStamina(replenishAmount);
             Debug.Log($"Player stamina replenished by {replenishAmount}.");
-            Destroy(gameObject);
+
+            Renderer rend = GetComponent<Renderer>();
+            if (rend != null)
+            {
+                rend.material.SetFloat("_IsFade", 1f);
+
+                rend.material.SetFloat("_StartTime", Time.time);
+            }
+            //wait for 0.5fs then destroy
+            Destroy(gameObject, 0.5f);
         }
     }
 }
